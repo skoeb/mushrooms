da80cf6f19301e7e8077ea2cedc6e8445132fcbf
skoeb <samkoebrich@gmail.com>
2020-11-06 00:05:09 -0700

    config api

--- api/config.py
+++ api/config.py
@@ -1,8 +1,8 @@
-PG_USER = 'mushroom-writer'
-PG_PASSWORD = '481224v!'
-PG_HOST = 'skoeb.ddns.net'
-PG_PORT = '5432'
-PG_DB = 'mushroom'
-PG_URI = f"postgresql://{PG_USER}:{PG_PASSWORD}@{PG_HOST}:{PG_PORT}/{PG_DB}"
+PG_USER = 
+PG_PASSWORD = 
+PG_HOST = 
+PG_PORT = 
+PG_DB = 
+PG_URI = 
 
-VALID_TOKENS = ['sam']
+VALID_TOKENS = []
